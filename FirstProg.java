2f43806b0f0e598dc4747f5651c9655df3c4fe33
ChrisGehrke <31970227+ChrisGehrke@users.noreply.github.com>
2017-09-19 14:24:03 -0600

    Add files via upload

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+  System.out.println( "I, Chris Gehrke, am determined to learn how to code." );
+  System.out.println( "Today's date is Monday, September 18, 2017." );
+  }
+}
